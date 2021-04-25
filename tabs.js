955edb6c1152b7b0b24027bf59f25c516c213a09
VDhriti <dhritiverma02@gmail.com>
2021-04-26 00:30:56 +0530

    Scoring and Tracking updated

--- tabs.js
+++ tabs.js
@@ -1,121 +1,84 @@
+var score = []; //the scores as per the option
+var option_chosen_array = []; // the option id via the getAttribute tag
+var dict = {
+    1: 1,
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 1,
+    6: 2,
+    7: 3,
+    8: 4,
+    9: 1,
+    10: 2,
+    11: 3,
+    12: 4,
+}
+var focus_log = [];
+var final_score = 0
+
 var currentTab = 0; // Current tab is set to be the first tab (0)
 showTab(currentTab)
 
 function showTab(n){
     var x = document.getElementsByClassName("test");
     var y = document.getElementsByClassName("btn-next-2")
-    if (x.length != y.length) {
+    if (x.length != y.length+1) {
         alert("ERROR")
     } else {
-        console.log(x.length)
-        console.log(y.length)
-
         for(i=0; i<x.length; i++){
             if(i!= n){
                x[i].style.display = "none";
-               y[i].style.display = "none";
-               console.log(i)
             } else {
                x[i].style.display = "block";
+            }
+        }
+        for(i=0; i<y.length; i++){
+            if(i!= n){
+               y[i].style.display = "none";
+            } else {
                y[i].style.display = "block";
-               console.log(i)
             }
         }
     }
 }
 
+//focus logger lol since radio buttons wont work
+function selectOption(){
+    var temp = document.getElementsByClassName("icon-box");
+    for(i=0; i<temp.length ;i++){
+        if(temp[i] === document.activeElement){{
+            focus_log.push(temp[i].getAttribute("id"));
+        }}
+    }
+    console.log(focus_log)    
+}
+
 function addCount(n){
-    currentTab = currentTab + n
+    //finding the last focused item and appending it
+    option_chosen_array.push(focus_log[focus_log.length-1]);
+    score.push(dict[focus_log[focus_log.length-1]]);
+    console.log(score)
+
+    currentTab = currentTab + n;
+    if (score.length != (currentTab)){
+        alert("Either you're cheating or there is a program error :(. In case of the latter you are kindly requested to contact the Cygna Space Society core team to report the issue.");
+    }
+
+    if (currentTab == document.getElementsByClassName("test").length-1){
+        //finding the total score i.e the sum of array components
+        for(i=0;i< (score.length); i++){
+        final_score = final_score + score[i];
+    }
+        function scoreDisplay(){
+            console.log("this half works:  ", final_score)
+                return(final_score);
+            }
+        
+        document.getElementById("final-thing").innerHTML = scoreDisplay();
+    }
+
     showTab(currentTab)
 }
 
 
-
-
-
-
-    // var currentTab = 0; // Current tab is set to be the first tab (0)
-    // var x = document.getElementsByClassName("tab");
-
-    // console.log(x)
-
-    // for(i=0; i<x; i++){
-    //   if(i!= currentTab){
-    //     x[i].style.display = "none";
-    //   } else {
-    //     x[i].style.display = "block";
-    //   }
-    // }
-
-
-    // showTab(currentTab); // Display the current tab
-    
-    // function showTab(n) {
-    //   // This function will display the specified tab of the form...
-    //   var x = document.getElementsByClassName("tab");
-    //   x[n].style.display = "block";
-    //   // ... and fix the Previous/Next buttons:
-    //   if (n == 0) {
-    //     document.getElementById("prevBtn").style.display = "none";
-    //   } else {
-    //     document.getElementById("prevBtn").style.display = "inline";
-    //   }
-    //   if (n == (x.length - 1)) {
-    //     document.getElementById("nextBtn").innerHTML = "Submit";
-    //   } else {
-    //     document.getElementById("nextBtn").innerHTML = "Next";
-    //   }
-    //   // ... and run a function that will display the correct step indicator:
-    //   fixStepIndicator(n)
-    // }
-    
-    // function nextPrev(n) {
-    //   // This function will figure out which tab to display
-    //   var x = document.getElementsByClassName("tab");
-    //   // Exit the function if any field in the current tab is invalid:
-    //   if (n == 1 && !validateForm()) return false;
-    //   // Hide the current tab:
-    //   x[currentTab].style.display = "none";
-    //   // Increase or decrease the current tab by 1:
-    //   currentTab = currentTab + n;
-    //   // if you have reached the end of the form...
-    //   if (currentTab >= x.length) {
-    //     // ... the form gets submitted:
-    //     document.getElementById("regForm").submit();
-    //     return false;
-    //   }
-    //   // Otherwise, display the correct tab:
-    //   showTab(currentTab);
-    // }
-    
-    // function validateForm() {
-    //   // This function deals with validation of the form fields
-    //   var x, y, i, valid = true;
-    //   x = document.getElementsByClassName("tab");
-    //   y = x[currentTab].getElementsByTagName("input");
-    //   // A loop that checks every input field in the current tab:
-    //   for (i = 0; i < y.length; i++) {
-    //     // If a field is empty...
-    //     if (y[i].value == "") {
-    //       // add an "invalid" class to the field:
-    //       y[i].className += " invalid";
-    //       // and set the current valid status to false
-    //       valid = false;
-    //     }
-    //   }
-    //   // If the valid status is true, mark the step as finished and valid:
-    //   if (valid) {
-    //     document.getElementsByClassName("step")[currentTab].className += " finish";
-    //   }
-    //   return valid; // return the valid status
-    // }
-    
-    // function fixStepIndicator(n) {
-    //   // This function removes the "active" class of all steps...
-    //   var i, x = document.getElementsByClassName("step");
-    //   for (i = 0; i < x.length; i++) {
-    //     x[i].className = x[i].className.replace(" active", "");
-    //   }
-    //   //... and adds the "active" class on the current step:
-    //   x[n].className += " active";
-    // }
